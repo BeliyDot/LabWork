06da5085811ef8b66254b588b0a09d1f64c8e09e
BeliyDot <artemonchannel2005@gmail.com>
2024-02-18 00:45:42 +0200

    Add main.cpp of example classes usage

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "user.h"
+#include "library.cpp"
+#include "game.cpp"
+#include "user.cpp"
+using namespace std;
+
+int main()
+{
+    cout << "Example program." << endl;
+    cout << "another example." << endl;
+    User dummy;
+    dummy.printLibrary();
+    User someone("Ub1y Canegrov", "someone@mymail.com", "123452");
+    someone.printLibrary();
+    Game game1("Minecraft", "path/to/minecraft.exe");
+    someone.addGame(game1);
+    someone.printLibrary();
+}
