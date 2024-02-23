74ef8d652a14e12259d97fd132acbf6c6a3845e4
BeliyDot <artemonchannel2005@gmail.com>
2024-02-23 09:59:13 +0200

    Add lines to demonstrate const object usage

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,17 @@ using namespace std;
 
 int main()
 {
-    cout << "Example program." << endl;
-    cout << "another example." << endl;
     User dummy;
     dummy.printLibrary();
+    
     User someone("Ub1y Canegrov", "someone@mymail.com", "123452");
     someone.printLibrary();
+    
     Game game1("Minecraft", "path/to/minecraft.exe");
+    const Game game2("Need For Speed Most Wanted", "path/to/speed.exe", "Racing");
+    // game2.rename("Need For Speed Underground");
+    
     someone.library->addGame(game1);
+    someone.library->addGame(game2);
     someone.printLibrary();
 }
