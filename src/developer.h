ad65c0e555fa894d9b57e662c759acd459c7a4a0
BeliyDot <artemonchannel2005@gmail.com>
2024-03-06 12:13:50 +0200

    Add Developer class, derived from User

---
+++ src/developer.h
@@ -0,0 +1,16 @@
+#ifndef LABWORK_DEVELOPER_H
+#define LABWORK_DEVELOPER_H
+
+#include "devgame.h"
+#include "user.h"
+
+class Developer : public User {
+private:
+    vector<DevGame> devLib;
+public:
+    void addDevGame(DevGame game) { devLib.push_back(game); }
+    Developer(string &username, string &email, string &password, Person persona = Person()):
+        User(username, email, password, persona) {}
+};
+
+#endif
