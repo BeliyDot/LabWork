865467efe1a093573f9bae7cae6980625a3958b8
BeliyDot <artemonchannel2005@gmail.com>
2024-03-08 10:24:11 +0200

    Update main.cpp file for additional tests

--- src/main.cpp
+++ src/main.cpp
@@ -12,10 +12,31 @@ int main()
     someone.printLibrary();
     
     Game game1("Minecraft", "path/to/minecraft.exe");
-    const Game game2("Need For Speed Most Wanted", "path/to/speed.exe", "Racing");
+    Game game2("Need For Speed Most Wanted", "path/to/speed.exe", "Racing");
     // game2.rename("Need For Speed Underground");
     
     someone.library->addGame(game1);
     someone.library->addGame(game2);
     someone.printLibrary();
+    
+    cout << someone.library->hasGame(game1);
+
+    Person artem = Person("Artem", "Biliakov", "Evgenovich");
+    User belDot("beldot", "bruhers@gmai.com", "1234", artem);
+    belDot.printInfo();
+    belDot.printLibrary();
+    belDot.printCollections();
+    
+    belDot.library->addGame(game1);
+    belDot.library->addGame(game2);
+
+    string name = "Need For Speed";
+    belDot.createCollection(name, vector<Game>());
+    cout << belDot.library->hasGame(game2) << endl;
+    belDot.addToCollection(name, game2);
+
+    belDot.printInfo();
+    belDot.printLibrary();
+    belDot.printCollections();
+    
 }
