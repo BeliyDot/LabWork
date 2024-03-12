bac924a4fb2ba9f668b86a53c16e69e57b92ff58
BeliyDot <artemonchannel2005@gmail.com>
2024-03-12 09:29:49 +0200

    Override two virtual methods in Developer class

--- src/developer.h
+++ src/developer.h
@@ -2,6 +2,7 @@
 #define LABWORK_DEVELOPER_H
 
 #include "devgame.h"
+#include "library.h"
 #include "user.h"
 
 class Developer final : public User {
@@ -9,6 +10,9 @@ private:
     vector<DevGame> devLib;
 public:
     void addDevGame(DevGame game) { devLib.push_back(game); }
+    virtual void description() override { cout << "I am a game library user and also a developer. My username is " << getUsername() << '.' << endl; }
+    virtual void hasLibrary() override { cout << "I have a game library. It contains " << library->gameCount() << " games. Also I have " << devLib.size()
+    << " games in development." << endl; }
     Developer(string &username, string &email, string &password, Person persona = Person()):
         User(username, email, password, persona) {}
 };
